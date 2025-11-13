2b146b1be1f0a8ae4353a759bdad2e55ea9c4804
rjsilva <silvarjronaldo@gmail.com>
2025-11-12 23:34:31 -0300

    feat: business logic

---
+++ rest-spring/src/main/java/br/com/rjs/rest_spring/RestSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.rjs.rest_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringApplication.class, args);
+	}
+
+}
---
+++ rest-spring/src/test/java/br/com/rjs/rest_spring/RestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rjs.rest_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
