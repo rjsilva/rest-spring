78c949bd5c5e9b914dd4b9c6bde159e7e8dcd807
rjsilva <silvarjronaldo@gmail.com>
2025-12-04 19:53:00 -0300

    feat: add rules in response

--- rest-spring/src/main/java/br/com/rjs/rest_spring/dto/PersonResponseDto.java
+++ rest-spring/src/main/java/br/com/rjs/rest_spring/dto/PersonResponseDto.java
@@ -1,12 +1,16 @@
 package br.com.rjs.rest_spring.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.Date;
 
 public record PersonResponseDto(
         Long id,
         String  name,
+        Character gender,
         String address,
-        Date birthDay,
-        Character gender
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        Date birthDay
 ) {
 }
